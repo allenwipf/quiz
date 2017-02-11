b8dc3d8d61c34f4dd808aa2006bdf57597f921cf
Allen Wipf <allenwipf@msn.com>
2017-02-10 21:23:15 -0600

    mostly finished

--- scripts.js
+++ scripts.js
@@ -1,59 +1,56 @@
 window.addEventListener("load", function () {
 
 
-	function postLikeListener(){
+	function questionSubmitListener(){
 		document.getElementsByClassName("question")[0].addEventListener("submit", question);
 	}
 
-	function startGameListener(){
-		document.getElementById("start").addEventListener("click", startGame);
-	}
 
 	function restartGameListener(){
 
-		document.getElementById("restart").addEventListener("click", restartGame);
+		document.getElementById("start").addEventListener("click", restartGame);
 	}
 
-	postLikeListener();
-	startGameListener();
+	questionSubmitListener();
 	restartGameListener();
 	
 
 })
 
-var questions = ["first letter of alphebet" , "last letter of alphebet", "What is my name"];
-var answers = ["a","z", "allen"];
+var questions = ["first letter of alphebet" , "last letter of alphebet", "What is 1 + 1"];
+var answers = ["a","z", 2];
 var questionsAsked = 0
 var questionsWrong = 0 
    
 
-
+//This fuction sets some node values so the page doesn't haven't to be refreshed to 
+//begin a new game
 function restartGame(){
+	
+	document.getElementsByClassName("question")[0].style.display = "block"
+	document.getElementsByClassName("submission")[0].style.display = "block"
+	document.getElementById("checkAnswer").innerHTML = "Good Luck!"
+	document.getElementById("qnumber").innerText = 0
+	document.getElementById("score").innerHTML = ''
+	questionsAsked = 0
+	questionsWrong = 0 
 
-	location.reload();
-
+	startGame()
 }
 
 function startGame(e){
 
-	var qNum = parseInt(document.getElementById("qnumber").innerText)
-	
+	var qNum = parseInt(document.getElementById("qnumber").innerText);
 	var x;
 
 	if (qNum == 0){
 		document.getElementById("ask").innerHTML = questions[0];
-		document.getElementById("qnumber").innerText = 1;
+		document.getElementById("qnumber").innerText = 1
 	
 	} else if (qNum < questions.length) {
 		document.getElementById("ask").innerHTML = questions[qNum];
-		document.getElementById("qnumber").innerText = qNum + 1;
-
-    } else {
-
-    	return
-    }
-
-	e.preventDefault();
+		document.getElementById("qnumber").innerText = qNum + 1
+	}
 }
 
 
@@ -62,22 +59,22 @@ function question(e){
 	var qNum = parseInt(document.getElementById("qnumber").innerText) -1
 	var answer = document.getElementsByClassName("input")[0].value
 
-	if (answer == answers[qNum]){
+	if (answer.toLowerCase() == answers[qNum]){
 		document.getElementsByClassName("input")[0].value = ''
-		outPutResult("correct")
+		outputResults("correct")
 		startGame(e);
 
 	} else {
 		document.getElementsByClassName("input")[0].value = ''
-		outPutResult("wrong")
+		outputResults("wrong")
 		startGame(e);
 	}
-	e.preventDefault()
+	 e.preventDefault(e)
 }
 
 
-function outPutResult(answer){
-
+function outputResults(answer){
+	
 	if (answer == "correct") {
 
 		questionsAsked += 1
@@ -88,16 +85,21 @@ function outPutResult(answer){
 		questionsWrong += 1
 	}
 
-	if (questionsAsked == questions.length) 
-
-		document.getElementById("checkAnswer").innerHTML = ("Out of  " + questionsAsked + ", you got " 
-		+ (questionsAsked - questionsWrong) + " correct.")
-	    + " Your score is " + (Math.floor(((questionsAsked - questionsWrong) / questionsAsked) * 100)) + "%"
-
+	declareWinner()
 }
 
 
+function declareWinner(){
 
+	if (questionsAsked >= questions.length){
+
+	document.getElementById("checkAnswer").innerHTML = ("Out of  " + questionsAsked + ", you got " 
+	+ (questionsAsked - questionsWrong) + " correct.")
+	document.getElementById("score").innerHTML = " Your score is " + (Math.floor(((questionsAsked - questionsWrong) / questionsAsked) * 100)) + "%"
+
+	document.getElementsByClassName("question")[0].style.display = "none"
+	}	
+}
 
 
 
